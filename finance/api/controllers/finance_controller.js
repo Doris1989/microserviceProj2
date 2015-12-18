dc43faf5c7781344f11e5f12c160039e4ffce38e
Doris1989 <dorisforever1989@gmail.com>
2015-12-17 22:35:08 -0500

    add support for REST API

--- finance/api/controllers/finance_controller.js
+++ finance/api/controllers/finance_controller.js
@@ -8,6 +8,11 @@ module.exports = {
   updateFinanceRecord: updateFinanceRecord,
   deleteFinanceRecord: deleteFinanceRecord,
   createFinanceRecord: createFinanceRecord,
+  swaggergetFinanceRecords: swaggergetFinanceRecords,
+  swaggergetFinanceRecord: swaggergetFinanceRecord,
+  swaggercreateFinanceRecord: swaggercreateFinanceRecord,
+  swaggerupdateFinanceRecord: swaggerupdateFinanceRecord,
+  swaggerdeleteFinanceRecord: swaggerdeleteFinanceRecord 
 };
 
 
@@ -173,5 +178,105 @@ function deleteFinanceRecord(msg) {
         console.log(res);
       });
     }  
+  });
+}
+
+
+function swaggergetFinanceRecords(req, res, next) {
+  //console.log(request);
+  Finance.find({}, function(err, data) {
+    if(err) return next(err);
+    res.json(data);
+  });
+}
+
+
+function swaggergetFinanceRecord(req, res, next) { //get finance record by student id and school id
+  var stuId = req.swagger.params.studentId.value;
+  var schId = req.swagger.params.schoolId.value;
+  Finance.find({_id : stuId, schoolId : schId}, function(err, data) {
+    if(err) return next(err);
+    else if(!data || data.length == 0) {
+      var error = new Error('No finance record found. Bad Request.');
+      error.statusCode = 400;
+      return next(error);
+    }
+    res.status(200);
+    res.json(data);
+  });
+}
+
+
+function swaggercreateFinanceRecord(req, res, next) {
+  var body = req.swagger.params.financerecord.value;
+  var newRecord = new Finance({ _id : body.studentId,
+                                schoolId : body.schoolId,
+                                schoolName : body.schoolName,
+                                studentName : body.studentName,
+                                tuition : body.tuition,
+                                loan : body.loan,
+                                insurance : body.insurance,
+                                equipmentFee : body.equipmentFee,
+                                awards : body.awards});
+  Finance.find({_id : body.studentId, schoolId : body.schoolId}, function(err, data) {
+    if(err) return next(err);
+    else if(data.length != 0) {
+      var error = new Error("Duplicate record found. Bad Request.");
+      error.statusCode = 400;
+      return next(error);
+    }
+    else {
+      newRecord.save(function(err, newRecord, data) {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(201);
+        res.json("Successfully created new finance record.");
+      });
+    }
+  });
+}
+
+
+
+function swaggerupdateFinanceRecord(req, res, next) {
+  var body = req.swagger.params.financerecord.value;
+  Finance.update({_id : body.studentId, schoolId : body.schoolId}, 
+                              { schoolName : body.schoolName,
+                                studentName : body.studentName,
+                                tuition : body.tuition,
+                                loan : body.loan,
+                                insurance : body.insurance,
+                                equipmentFee : body.equipmentFee,
+                                awards : body.awards}, function(err, data) {
+                                  if(data['nModified'] == 0) {
+                                    var error = new Error("No finance record found. Cannot do update. Bad Request.");
+                                    error.statusCode = 400;
+                                    return next(error);
+                                  }
+                                  if(err) return next(err);
+                                  res.status(204); //put
+                                  res.setHeader('Content-Type', 'application/json');
+                                  res.json("Successfully update finance record.");
+  });
+}
+
+
+
+function swaggerdeleteFinanceRecord(req, res, next) {
+  Finance.find({_id : req.swagger.params.studentId.value, schoolId: req.swagger.params.schoolId.value}, function (err, data) {
+    if(err) return next(err);
+    else if(!data || data.length == 0) {
+      var error = new Error ("Duplicate finance record found. Bad Request.");
+      error.statusCode = 400;
+      return next(error);
+    }
+    else {
+      Finance.remove({_id: req.swagger.params.studentId.value, schoolId : req.swagger.params.schoolId.value}, function (err, data) {
+        if(err) return next(err);
+        else {
+          res.status(204);
+          res.json("Successfully delete finance record.");
+        }
+      });
+    }
   });
 }
